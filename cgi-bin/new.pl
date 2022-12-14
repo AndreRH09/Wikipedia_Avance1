4d059fcd2c33d5651d86919b4d09549090f41331
AndreR <aanazcph@unsa.edu.pe>
2022-12-14 12:22:46 -0500

    ultimos cambios

--- cgi-bin/new.pl
+++ cgi-bin/new.pl
@@ -3,12 +3,12 @@ use strict;
 use warnings;
 use CGI;
 
-my $q = CGI->new;
-my $epq = CGI->new;
+my $tit = CGI->new;
+my $text = CGI->new;
 
 
-my $titulo = $q->param('titulo');
-my $texto = $epq->param('texto');
+my $titulo = $tit->param('titulo');
+my $texto = $text->param('texto');
 
 # $all =~ s/ /+/g;
 # $exact =~ s/ /+/g;
@@ -27,6 +27,7 @@ print<<BLOCK;
   <body>
     <h1>$titulo</h1>
     <p>$texto</p>
+    <hr style="width:70%;text-align:left;margin-left:0">
     <h2>Pagina grabada <a
         href="list.pl"> Listado paginas</a> </h2>
   </body>
