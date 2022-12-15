b02be925e53ea09143203332baee377792c1010d
AndreR <aanazcoh@gmail.com>
2022-12-15 13:07:42 -0500

    base de datos registra

--- cgi-bin/new.pl
+++ cgi-bin/new.pl
@@ -2,21 +2,20 @@
 use strict;
 use warnings;
 use CGI;
+use DBI;
 
-my $tit = CGI->new;
-my $text = CGI->new;
-
+my $q = CGI->new;
 print "Content-type:text/html\r\n\r\n";
-my $titulo = $tit->param('titulo');
-my $markdown = $text->param('texto');
+my $titulo = $q->param('titulo');
+my $markdown = $q->param('texto');
 
 # SegundoMarkdown::
 my $title = $q->param('title');
 my $markdown2 = $q->param('markdown2');
 
-my $user = 'aanazcoh';
-my $password = '12345';
-my $dsn = "DBI:MariaDB:database=Lab_Pweb;host=localhost";
+my $user = 'alumno';
+my $password = 'pweb1';
+my $dsn = "DBI:MariaDB:database=pweb1;host=localhost";
 my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
 
 if((defined($titulo) and defined($markdown)) or (!defined($title) and !defined($markdown2))){
